9b8ecd7a246b7d12dd64a2d756e17c70822cf485
Dmitrii Korotun <korotundm@gmail.com>
2020-05-07 00:14:23 +0300

    Client: Added classes for diagrammBuilding, added CartesianChart on main window

---
+++ Forecaster.Client/Drawing/DiagrammBuilder.cs
@@ -0,0 +1,58 @@
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Forecaster.Client.Drawing
+{
+    class DiagrammBuilder
+    {
+        public SeriesCollection GetSeriesCollection(CartesianMapper<DateModel> mapper, params Dictionary<DateTime, double>[] dataSeries)
+        {
+            SeriesCollection seriesCollection = new SeriesCollection(mapper);
+
+            foreach (Dictionary<DateTime, double> diagrammData in dataSeries)
+            {
+                LineSeries lineSeries = CreateLineSeries(diagrammData);
+
+                seriesCollection.Add(lineSeries);
+            }
+
+            return seriesCollection;
+        }
+
+        private LineSeries CreateLineSeries(Dictionary<DateTime, double> diagrammData)
+        {
+            return new LineSeries
+            {
+                Values = ConvertToDateModel(diagrammData),
+                PointGeometry = null,
+                Fill = Brushes.Transparent
+            };
+        }
+
+        private ChartValues<DateModel> ConvertToDateModel(Dictionary<DateTime, double> dateDecimalDictionary)
+        {
+            var values = new ChartValues<DateModel>();
+
+            foreach (KeyValuePair<DateTime, double> dateDecimalPair in dateDecimalDictionary)
+            {
+                var dateModel = new DateModel
+                {
+                    Date = dateDecimalPair.Key,
+                    Value = dateDecimalPair.Value
+                };
+
+                values.Add(dateModel);
+            }
+
+            return values;
+        }
+    }
+}

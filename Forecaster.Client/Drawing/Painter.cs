9bc685f9378f0a7df8f95eff986c865bf6fe208e
Dmitrii Korotun <korotundm@gmail.com>
2020-05-08 00:44:05 +0300

    Added painter and converter classes

---
+++ Forecaster.Client/Drawing/Painter.cs
@@ -0,0 +1,23 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecaster.Client.Drawing
+{
+    public class Painter
+    {
+        public SeriesCollection Series { get; set; }
+
+        public void AddLineSeries(params Dictionary<DateTime, double>[] csvStockList)
+        {
+            IEnumerable<LineSeries> newLineSeriesRange = new DiagrammBuilder().CreateLineSeriesRange(csvStockList);
+
+            foreach (LineSeries newLineSeries in newLineSeriesRange)
+                Series.Add(newLineSeries);
+        }
+    }
+}
---
+++ Forecaster.Client/Drawing/ResponseConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecaster.Client.Drawing
+{
+    public static class ResponseConverter
+    {
+        public static Dictionary<DateTime, double> ConvertResponsePredictions(Dictionary<string, string> responsePredictions)
+        {
+            Dictionary<DateTime, double> convertedPredictions = new Dictionary<DateTime, double>(responsePredictions.Count);
+
+            foreach(KeyValuePair<string, string> prediction in responsePredictions)
+            {
+                KeyValuePair<DateTime, double> convertedPrediction = ConvertResponsePrediction(prediction);
+
+                convertedPredictions.Add(convertedPrediction.Key, convertedPrediction.Value);
+            }
+
+            return convertedPredictions;
+        }
+        
+        private static KeyValuePair<DateTime, double> ConvertResponsePrediction(KeyValuePair<string, string> prediction)
+        {
+            DateTime date = DateTime.Parse(prediction.Key);
+
+            double close = double.Parse(prediction.Value);
+
+            return new KeyValuePair<DateTime, double>(date, close);
+        }
+    }
+}

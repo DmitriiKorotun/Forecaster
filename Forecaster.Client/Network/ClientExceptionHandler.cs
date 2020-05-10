e89bed603f6af9633408b233a26aad2bc0680d67
Dmitrii Korotun <korotundm@gmail.com>
2020-05-10 03:15:45 +0300

    Added some exception handling in async client class

---
+++ Forecaster.Client/Network/ClientExceptionHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecaster.Client.Network
+{
+    class ClientExceptionHandler
+    {
+        public event EventHandler<ExceptionReportEventArgs> ExceptionReport;
+
+        public List<Exception> RaisedExceptions { get; private set; }
+
+        public ClientExceptionHandler()
+        {
+            RaisedExceptions = new List<Exception>();
+        }
+
+        public ClientExceptionHandler(Exception execption)
+        {
+            RaisedExceptions = new List<Exception>
+            {
+                execption
+            };
+        }
+
+        public ClientExceptionHandler(List<Exception> exceptions)
+        {
+            RaisedExceptions = exceptions;
+        }
+
+        private void OnExceptionReport(object sender, Exception exception)
+        {
+            ExceptionReport?.Invoke(sender, new ExceptionReportEventArgs(exception));
+        }
+    }
+}
